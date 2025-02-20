925b8add3a4dc2ced6815806c2349218a40a9cac
apurvasankhe1338 <apurvaasankhe1338@gmail.com>
2025-02-20 15:20:11 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const root = document.getElementById('root');
+if (!root) throw new Error('Root element not found');
+
+const rootElement = createRoot(root);
+rootElement.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
